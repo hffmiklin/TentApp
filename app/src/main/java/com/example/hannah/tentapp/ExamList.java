cee39e84a21c6892aff08403f4ac2d3f079eb6ae
hffmiklin <hannah.miklin@gmail.com>
2016-05-06 19:53:53 +0200

    Started building a hard coded list of courses that will be taken from db in the future, to see if connection works

--- app/src/main/java/com/example/hannah/tentapp/ExamList.java
+++ app/src/main/java/com/example/hannah/tentapp/ExamList.java
@@ -2,6 +2,7 @@ package com.example.hannah.tentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
 
 public class ExamList extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class ExamList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_list);
+
+        // TODO Import list of exams from database
+        // hard coding list of strings instead of getting from db
+        // just to see if "future" connection works
+        List<String> courseList = new ArrayList<String>();
+        courseList.add("TIG164 Interaktionsdesign");
+        courseList.add("TIG015 Informationsteknologi och informationssystem");
+        courseList.add("TIG016 Verksamheter och information");
+        courseList.add("TIG058 Databaser och programmeringsteknik");
+        courseList.add("TIG999 Harry Potter och hans världar");
+        courseList.add("TIG059 Systemutvecklingsprojekt");
+        courseList.add("TIG098 eBusiness och eGovernment");
+        courseList.add("TIG162 Affärssystem");
+        courseList.add("TIG163 Beslutsstödssystem");
+
     }
 }
