c6be66bc2fa8f978161f7d2aed2239112c9e6c4c
hffmiklin <hannah.miklin@gmail.com>
2016-05-14 22:53:35 +0200

    Gjort om knappar för tentamen, fixat informationsfönstret och kopplat så all information om specifik tenta kommer in i fönstret, formaterat text i fönstret

--- app/src/main/java/com/example/hannah/tentapp/ExamInformation.java
+++ app/src/main/java/com/example/hannah/tentapp/ExamInformation.java
@@ -2,20 +2,58 @@ package com.example.hannah.tentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ExamInformation extends AppCompatActivity {
+
+    TextView courseName, courseDate, courseTime, courseAid, coursePlace, courseRegInt, courseRegYN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_information);
 
+
+        courseName = (TextView) findViewById(R.id.courseNameTextView);
+        courseDate = (TextView) findViewById(R.id.dateTextView);
+        courseTime = (TextView) findViewById(R.id.courseTimeTextView);
+        courseAid = (TextView) findViewById(R.id.courseAidTextView);
+        coursePlace = (TextView) findViewById(R.id.coursePlaceTextView);
+        courseRegInt = (TextView) findViewById(R.id.courseRegIntervalTextView);
+        courseRegYN = (TextView) findViewById(R.id.courseRegTextView);
+
+        Bundle extras = getIntent().getExtras();
+
+        String courseNameString = extras.getString("course_name");
+        String courseDateString = extras.getString("course_date");
+        String courseTimeString = extras.getString("course_time");
+        String courseAidString = extras.getString("course_aid");
+        String coursePlaceString = extras.getString("course_place");
+        String courseRegIntervallString = extras.getString("course_reg_intervall");
+        String courseRegYesNoString = extras.getString("course_reg_yes_no");
+
+        courseName.setText(courseNameString);
+        courseDate.setText("Datum: " + courseDateString);
+        courseTime.setText("Tid: " + courseTimeString);
+        courseAid.setText("Hjälpmedel: " + courseAidString);
+        coursePlace.setText("Lokal: " + coursePlaceString);
+        courseRegInt.setText("Registrering: " + courseRegIntervallString);
+        courseRegYN.setText("Anmäld: " + courseRegYesNoString);
+
+
+
+
         // TODO import list of elements of chosen exam, connect with db
-        // anmälningsintervall
+
         // datum
         // tidstart + slut
         // hjälpmedel
         // sal, hus (om finns)
+        // anmälningsintervall
+        //anmäld eller inte
         // knapp: hitta hus+sal
+
+
+
     }
 }
--- app/src/main/java/com/example/hannah/tentapp/ExamList.java
+++ app/src/main/java/com/example/hannah/tentapp/ExamList.java
@@ -1,5 +1,6 @@
 package com.example.hannah.tentapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +8,22 @@ import java.util.*;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.graphics.drawable.*;
+import android.view.View;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExamList extends AppCompatActivity {
+
+    List<String> courseList;
+    List<String> courseDateList;
+    List<String> courseTimeList;
+    List<String> courseAidList;
+    List<String> coursePlaceList;
+    List<String> courseRegIntervalList;
+    List<String> courseRegYesNoList;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +36,119 @@ public class ExamList extends AppCompatActivity {
         // hard coding list of strings instead of getting from db
         // just to see if "future" connection works
 
-        List<String> courseList = new ArrayList<String>();
-        courseList.add("TIG164 \n Interaktionsdesign");
-        courseList.add("TIG015 \n Informationsteknologi och informationssystem");
-        courseList.add("TIG016 \n Verksamheter och information");
-        courseList.add("TIG058 \n Databaser och programmeringsteknik");
-        courseList.add("TIG999 \n Harry Potter och hans världar");
-        courseList.add("TIG059 \n Systemutvecklingsprojekt");
-        courseList.add("TIG098 \n eBusiness och eGovernment");
-        courseList.add("TIG162 \n Affärssystem");
-        courseList.add("TIG163 \n Beslutsstödssystem");
+        courseList = new ArrayList<String>();
+        courseList.add("TIG164 Interaktionsdesign");
+        courseList.add("TIG015 Informationsteknologi och informationssystem");
+        courseList.add("TIG016 Verksamheter och information");
+        courseList.add("TIG058 Databaser och programmeringsteknik");
+        courseList.add("TIG999 Harry Potter och hans världar");
+        courseList.add("TIG059 Systemutvecklingsprojekt");
+        courseList.add("TIG098 eBusiness och eGovernment");
+        courseList.add("TIG162 Affärssystem");
+        courseList.add("TIG163 Beslutsstödssystem");
+
+        courseDateList = new ArrayList<String>();
+        courseDateList.add("2016-06-03");
+        courseDateList.add("2016-05-22");
+        courseDateList.add("2016-05-17");
+        courseDateList.add("2016-04-22");
+        courseDateList.add("2016-04-04");
+        courseDateList.add("2016-03-21");
+        courseDateList.add("2016-03-10");
+        courseDateList.add("2016-02-19");
+        courseDateList.add("2016-02-16");
+
+        courseTimeList = new ArrayList<String>();
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("14.30-18.30");
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("14.30-18.30");
+        courseTimeList.add("8.30-12.30");
+        courseTimeList.add("8.30-12.30");
+
+        courseAidList = new ArrayList<String>();
+        courseAidList.add("Inget");
+        courseAidList.add("Inget");
+        courseAidList.add("Inget");
+        courseAidList.add("Miniräknare");
+        courseAidList.add("Kursbok");
+        courseAidList.add("Inget");
+        courseAidList.add("Kursbok");
+        courseAidList.add("Inget");
+        courseAidList.add("Inget");
+
+        coursePlaceList = new ArrayList<String>();
+        coursePlaceList.add("SAGA, sal 102");
+        coursePlaceList.add("PATRICIA, sal 107");
+        coursePlaceList.add("PATRICIA, sal 122");
+        coursePlaceList.add("SAGA, sal 109");
+        coursePlaceList.add("SAGA, sal 102");
+        coursePlaceList.add("PATRICIA, sal 101");
+        coursePlaceList.add("PATRICIA, sal 101");
+        coursePlaceList.add("PATRICIA, sal Omega");
+        coursePlaceList.add("SAGA, sal 103");
+
+        courseRegIntervalList = new ArrayList<String>();
+        courseRegIntervalList.add("2016-04-03 - 2016-05-03");
+        courseRegIntervalList.add("2016-03-22 - 2016-04-22");
+        courseRegIntervalList.add("2016-03-17 - 2016-04-17");
+        courseRegIntervalList.add("2016-02-22 - 2016-03-22");
+        courseRegIntervalList.add("2016-02-04 - 2016-03-04");
+        courseRegIntervalList.add("2016-01-21 - 2016-02-21");
+        courseRegIntervalList.add("2016-01-10 - 2016-02-10");
+        courseRegIntervalList.add("2015-12-19 - 2016-01-19");
+        courseRegIntervalList.add("2015-12-16 - 2016-01-16");
+
+        courseRegYesNoList = new ArrayList<String>();
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Nej");
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Ja");
+        courseRegYesNoList.add("Nej");
+        courseRegYesNoList.add("Ja");
+
+
 
         for(int i = 0; i< courseList.size(); i++) {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             Button courseButton = new Button(this);
-            courseButton.setText(courseList.get(i));
-            courseButton.setTextSize(20);
+            courseButton.setId(i);
+            final int id = courseButton.getId();
+
+            courseButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(ExamList.this,ExamInformation.class);
+                    intent.putExtra("course_name", courseList.get(id));
+                    intent.putExtra("course_date", courseDateList.get(id));
+                    intent.putExtra("course_time", courseTimeList.get(id));
+                    intent.putExtra("course_aid", courseAidList.get(id));
+                    intent.putExtra("course_place", coursePlaceList.get(id));
+                    intent.putExtra("course_reg_intervall", courseRegIntervalList.get(id));
+                    intent.putExtra("course_reg_yes_no", courseRegYesNoList.get(id));
+                    startActivity(intent);
+                }
+            });
+
 
             // Design button
+            String courseListName = courseList.get(i);
+            String[] stringParts = courseListName.split(" ");
+            String courseButtonText = stringParts[0] + "\n";
+
+            for(int j = 1; j<stringParts.length ; j++ ){
+                courseButtonText = courseButtonText +" "+ stringParts[j];
+            }
+            courseButton.setText(courseButtonText);
+            courseButton.setTextSize(20);
             GradientDrawable drawable = new GradientDrawable();
             drawable.setShape(GradientDrawable.RECTANGLE);
             drawable.setStroke(5, Color.WHITE);
