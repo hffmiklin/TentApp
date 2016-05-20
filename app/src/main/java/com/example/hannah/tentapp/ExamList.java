142fb2c34f9ccc3de079840b30749ba32f0f701d
emelifransson <gusfraemh@student.gu.se>
2016-05-20 06:14:06 +0200

    Mark exam address automatically in map

--- app/src/main/java/com/example/hannah/tentapp/ExamList.java
+++ app/src/main/java/com/example/hannah/tentapp/ExamList.java
@@ -29,6 +29,7 @@ public class ExamList extends AppCompatActivity {
     List<String> courseRegOpenList;
     List<String> courseRegCloseList;
     List<String> courseRegYesNoList;
+    List<String> courseBuildingList;
 
     String userNameString;
 
@@ -69,105 +70,8 @@ public class ExamList extends AppCompatActivity {
         courseRegYesNoList = new ArrayList<String>();
         courseRegYesNoList = helper.getRegistered();
 
-        Log.d("What's in courseList?", courseList.get(0)+","+courseList.get(1));
-        //Debug
-        //String check = courseList.get(0);
-        //Log.d("Got any courses?", check);
-
-        // TODO Import list of exams from database
-        // hard coding list of strings instead of getting from db
-        // just to see if "future" connection works
-
-        /*
-        courseList = new ArrayList<String>();
-        courseList.add("TIG164 Interaktionsdesign");
-        courseList.add("TIG015 Informationsteknologi och informationssystem");
-        courseList.add("TIG016 Verksamheter och information");
-        courseList.add("TIG058 Databaser och programmeringsteknik");
-        courseList.add("TIG999 Harry Potter och hans världar");
-        courseList.add("TIG059 Systemutvecklingsprojekt");
-        courseList.add("TIG098 eBusiness och eGovernment");
-        courseList.add("TIG162 Affärssystem");
-        courseList.add("TIG163 Beslutsstödssystem");
-
-        courseDateList = new ArrayList<String>();
-        courseDateList.add("2016-06-03");
-        courseDateList.add("2016-05-22");
-        courseDateList.add("2016-05-17");
-        courseDateList.add("2016-04-22");
-        courseDateList.add("2016-04-04");
-        courseDateList.add("2016-03-21");
-        courseDateList.add("2016-03-10");
-        courseDateList.add("2016-02-19");
-        courseDateList.add("2016-02-16");
-
-        courseTimeList = new ArrayList<String>();
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("14.30-18.30");
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("14.30-18.30");
-        courseTimeList.add("8.30-12.30");
-        courseTimeList.add("8.30-12.30");
-
-        courseAidList = new ArrayList<String>();
-        courseAidList.add("Inget");
-        courseAidList.add("Inget");
-        courseAidList.add("Inget");
-        courseAidList.add("Miniräknare");
-        courseAidList.add("Kursbok");
-        courseAidList.add("Inget");
-        courseAidList.add("Kursbok");
-        courseAidList.add("Inget");
-        courseAidList.add("Inget");
-
-        coursePlaceList = new ArrayList<String>();
-        coursePlaceList.add("SAGA, sal 102");
-        coursePlaceList.add("PATRICIA, sal 107");
-        coursePlaceList.add("PATRICIA, sal 122");
-        coursePlaceList.add("SAGA, sal 109");
-        coursePlaceList.add("SAGA, sal 102");
-        coursePlaceList.add("PATRICIA, sal 101");
-        coursePlaceList.add("PATRICIA, sal 101");
-        coursePlaceList.add("PATRICIA, sal Omega");
-        coursePlaceList.add("SAGA, sal 103");
-
-        courseRegIntervalList1 = new ArrayList<String>();
-        courseRegIntervalList1.add("2016-04-03");
-        courseRegIntervalList1.add("2016-03-22");
-        courseRegIntervalList1.add("2016-03-17");
-        courseRegIntervalList1.add("2016-02-22");
-        courseRegIntervalList1.add("2016-02-04");
-        courseRegIntervalList1.add("2016-01-21");
-        courseRegIntervalList1.add("2016-01-10");
-        courseRegIntervalList1.add("2015-12-19");
-        courseRegIntervalList1.add("2015-12-16");
-
-        courseRegIntervalList2 = new ArrayList<String>();
-        courseRegIntervalList2.add("2016-05-03");
-        courseRegIntervalList2.add("2016-04-22");
-        courseRegIntervalList2.add("2016-04-17");
-        courseRegIntervalList2.add("2016-03-22");
-        courseRegIntervalList2.add("2016-03-04");
-        courseRegIntervalList2.add("2016-02-21");
-        courseRegIntervalList2.add("2016-02-10");
-        courseRegIntervalList2.add("2016-01-19");
-        courseRegIntervalList2.add("2016-01-16");
-
-        courseRegYesNoList = new ArrayList<String>();
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Nej");
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Ja");
-        courseRegYesNoList.add("Nej");
-        courseRegYesNoList.add("Ja");
-*/
-
+        courseBuildingList = new ArrayList<String>();
+        courseBuildingList = helper.getBuildings();
 
         for(int i = 0; i< courseList.size(); i++) {
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
@@ -189,6 +93,7 @@ public class ExamList extends AppCompatActivity {
                     intent.putExtra("course_reg_open", courseRegOpenList.get(id));
                     intent.putExtra("course_reg_close", courseRegCloseList.get(id));
                     intent.putExtra("course_reg_yes_no", courseRegYesNoList.get(id));
+                    intent.putExtra("course_building", courseBuildingList.get(id));
                     startActivity(intent);
                 }
             });
